4788b2b067badfb414d6d801a29f69fb9cc4251c
bitmonkLab3x10 <9x3whale@gmail.com>
2025-03-27 20:36:06 +0530

    Update connection.js
    Just check

--- connection.js
+++ connection.js
@@ -20,11 +20,12 @@ require('dotenv').config(); // Only needed for local development
 
 // Debug: Log the environment variables
 console.log("Environment Variables:", {
-  MONGODB_URI: process.env.MONGODB_URI,
+  // MONGODB_URI: process.env.MONGODB_URI,  // Removed added URL  and below changed Connection String
   DATABASE: process.env.DATABASE
 });
 
-const connectionString = process.env.MONGODB_URI; // Changed from DATABASE to MONGODB_URI
+// const connectionString = process.env.MONGODB_URI; // Changed from DATABASE to MONGODB_URI
+const connectionString = process.env.DATABASE;
 
 if (!connectionString) {
     console.error("MongoDB URI is missing. Check your environment variables.");
